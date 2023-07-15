7cc7a4b2d562be5eaf2d6fddcc60d2dfecd04103
h29839 <hikari9672@gmail.com>
2023-07-16 01:40:28 +0900

    as unknownの追加

--- src/components/utilAxios.tsx
+++ src/components/utilAxios.tsx
@@ -3,16 +3,20 @@ import { Axios } from "axios";
 const axios = new Axios({
   baseURL: "https://localhost:5000",
   headers: {
-    "Content-Type": "application/json"
+    "Content-Type": "application/json",
   },
   responseType: "json",
   withCredentials: true,
-  transformRequest: [function (data:unknown) {
-    return JSON.stringify(data);
-  }],
-  transformResponse: [function (data:string) {
-    return JSON.parse(data);
-  }]
+  transformRequest: [
+    function (data: unknown) {
+      return JSON.stringify(data);
+    },
+  ],
+  transformResponse: [
+    function (data: string) {
+      return JSON.parse(data) as unknown;
+    },
+  ],
 });
 
 export default axios;
