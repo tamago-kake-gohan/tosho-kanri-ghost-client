02c2595251fee3720c3c8a8492773ff19503edab
KITA-DS12 <kita.ds12@gmail.com>
2023-07-15 16:02:27 +0900

    feature: add component SelectGroup

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+import SelectGroup from "@/components/SelectGroup/SelectGroup";
+import "./home.css";
+
+const Main = () => {
+  return (
+    <div>
+      <SelectGroup />
+    </div>
+  );
+};
+
+export default Main;
