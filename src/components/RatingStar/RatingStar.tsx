89f2ee2f3387b2c8f3f7998fec58f4ff1fe3116e
h29839 <hikari9672@gmail.com>
2023-08-24 02:24:40 +0900

    色調整とサイズ調整

---
+++ src/components/RatingStar/RatingStar.tsx
@@ -0,0 +1,37 @@
+import "./RatingStar.css";
+
+interface RatingStarProps {
+  maxStars: number;
+  currentRating: number;
+  onStarClick: (rating: number) => void;
+}
+
+const RatingStar: React.FC<RatingStarProps> = ({
+  maxStars,
+  currentRating,
+  onStarClick,
+}) => {
+  const handleStarClick = (index: number) => {
+    onStarClick(index + 1);
+  };
+  return (
+    <div className="rating-stars">
+      {[...Array(maxStars)].map((_, index) => (
+        <svg
+          key={index}
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill={index < currentRating ? "#FFC107" : "#ACACAC"}
+          onClick={() => handleStarClick(index)}
+          className="star-icon"
+        >
+          <path d="M12 2a9 9 0 0 1 9 9v7.5a3.5 3.5 0 0 1-6.39 1.976a2.999 2.999 0 0 1-5.223 0a3.5 3.5 0 0 1-6.382-1.783L3 18.499V11a9 9 0 0 1 9-9Zm0 10c-1.105 0-2 1.12-2 2.5s.895 2.5 2 2.5s2-1.12 2-2.5s-.895-2.5-2-2.5ZM9.5 8a1.5 1.5 0 1 0 0 3a1.5 1.5 0 0 0 0-3Zm5 0a1.5 1.5 0 1 0 0 3a1.5 1.5 0 0 0 0-3Z" />
+        </svg>
+      ))}
+    </div>
+  );
+};
+
+export default RatingStar;
