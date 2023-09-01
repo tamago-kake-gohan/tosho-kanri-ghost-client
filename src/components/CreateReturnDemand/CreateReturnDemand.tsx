a25481d00d71e4870450668030f1e2d61d8695ad
XPA <xpadev@gmail.com>
2023-09-02 02:39:36 +0900

    refactor: create return demand button

--- src/components/CreateReturnDemand/CreateReturnDemand.tsx
+++ src/components/CreateReturnDemand/CreateReturnDemand.tsx
@@ -1,25 +1,17 @@
-import axios from "@/components/utilAxios";
 import Button from "@/components/Button/Button";
 
 type LendIdProps = {
   id: number;
+  email: string;//借りているユーザーのメールアドレス
+  name: string;//借りているユーザー名
+  title: string;//借りている本のタイトル
 };
 
-const CreateReturnDemand: React.FC<LendIdProps> = ({ id }) => {
-  async function sendReturnDemand() {
-    /*
-    propsで借りている人間のメールアドレスを取得できるようにする
-  */
-    await axios
-      .post("/api/v1/send_demand_letter", {
-        lendId: id,
-      })
-      .then((response) => {
-        console.log(response.data.message);
-      })
-      .catch((error) => {
-        console.log("エラー", error);
-      });
+const CreateReturnDemand = ({ email, name,title }:LendIdProps) => {
+  function sendReturnDemand() {
+    const link = document.createElement("a");
+    link.href = `mailto:${email}?subject=返却のお願い&body=${name}さん、こんにちは。「${title}」の返却をお願いします`;
+    link.click();
   }
   return (
     <div onClick={sendReturnDemand}>
